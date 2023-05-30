689d8b7bb1e03a5a6455a6e01844d10ab135dcc3
andressa <andressadejesusclementino@gmail.com>
2023-05-30 15:03:15 -0400

    Inicio do projeto de divisao de dois numeros

---
+++ git/Form1.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("informe o primeiro valor!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine( "informe o segundo valor!");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+Console.WriteLine(result);
